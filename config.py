07f45915a072249fa461c2e079dc073eb24feb18
Nakultt <nakulnuked@gmail.com>
2025-03-02 21:23:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    MODEL_PATH = "C:/Users/nakul/Desktop/New folder/movenet_lightning_4"
+    MODEL_PATH = # "PATH OF 'movenet_lightning_4' FOLDER" |  (THIS FOLDER WILL BE INSIDE REPO) | (EXAMPLE: "C:/Users/NAME/Desktop/fall_detection/movenet_lightning_4")
     VIDEO_SOURCE = "01.mp4"       
     INPUT_SIZE = (192, 192)
     CONFIDENCE_THRESHOLD = 0.2                    
